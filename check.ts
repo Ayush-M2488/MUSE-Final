6d9242c0d50e42925b98333962b79ada5047ed85
Ayush M <ayushm356@gmail.com>
2026-06-16 13:57:18 +0530

    chore: Clean up old diagnostic and testing scripts

--- check.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from './backend/config/db'; 
-async function run() { 
-  const student = await prisma.student.findFirst({ 
-    where: { user: { full_name: { contains: 'Punith', mode: 'insensitive' } } }, 
-    include: { user: true, enrollments: true, assessments: true } 
-  }); 
-  console.dir(student, { depth: null }); 
-} 
-run().finally(() => prisma.$disconnect());
